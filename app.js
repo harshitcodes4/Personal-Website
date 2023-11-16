38b9272047f3ebb72391ef97e798395c50822d4f
Harshit Saxena <146431234+saxenaharshit63@users.noreply.github.com>
2023-11-16 16:30:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// typed animation script
+let typed = new Typed("#element", {
+    strings: ["Frontend Developer", "And a Youtuber"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true, 
+});
+
+// slide in animation script
+const aboutSection = document.querySelector(".left");
+
+window.addEventListener('scroll', () => {
+    const sectionPosition = aboutSection.getBoundingClientRect();
+
+    if (sectionPosition.top <= window.innerHeight / 2) {
+        aboutSection.classList.add('active');
+    } else {
+        aboutSection.classList.remove('active');
+    }
+});
+
+// smooth scrolling script
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('.navlinks a');
+    
+    links.forEach(link => {
+        link.addEventListener('click', smoothScroll);
+    });
+    
+    function smoothScroll(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const yOffset = -65;
+        const y = targetElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
+        
+        window.scrollTo({
+            top: y,
+            behavior: 'smooth'
+        });
+    }
+});
